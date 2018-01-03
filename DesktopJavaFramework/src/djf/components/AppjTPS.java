2ba974b4fb950ef050db7396082f5c5f5222e20c
Qixin Chen <qchen5249@bths.edu>
2018-01-03 14:57:55 -0500

    Add files via upload

---
+++ DesktopJavaFramework/src/djf/components/AppjTPS.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package djf.components;
+
+/**
+ *
+ * @author qixin
+ */
+public interface AppjTPS {
+    public void doTransaction();
+    public void undoTransaction();
+    
+}
